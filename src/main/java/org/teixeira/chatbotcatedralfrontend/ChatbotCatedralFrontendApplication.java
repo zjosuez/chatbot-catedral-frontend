52e9760c6eaf26cc4b5d1bf39d0f4f41f3993f6f
Josué Teixeira <zjosuez@outlook.com>
2024-03-13 09:35:45 -0300

    feat: initial commit

---
+++ src/main/java/org/teixeira/chatbotcatedralfrontend/ChatbotCatedralFrontendApplication.java
@@ -0,0 +1,13 @@
+package org.teixeira.chatbotcatedralfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotCatedralFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotCatedralFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/teixeira/chatbotcatedralfrontend/ChatbotCatedralFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.teixeira.chatbotcatedralfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbotCatedralFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
