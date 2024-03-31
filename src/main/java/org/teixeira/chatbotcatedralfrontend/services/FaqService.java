a5e5fec1316dc8d8331e0822b05e9c5a57d6c0fc
Josué Teixeira <zjosuez@outlook.com>
2024-03-31 15:54:03 -0300

    feat: Enhancing validation for user input

--- src/main/java/org/teixeira/chatbotcatedralfrontend/services/FaqService.java
+++ src/main/java/org/teixeira/chatbotcatedralfrontend/services/FaqService.java
@@ -3,19 +3,34 @@ package org.teixeira.chatbotcatedralfrontend.services;
 import org.springframework.stereotype.Service;
 import org.teixeira.chatbotcatedralfrontend.domain.FaqAnswer;
 import org.teixeira.chatbotcatedralfrontend.utils.FaqAnswers;
+import java.text.Normalizer;
 import java.util.Arrays;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
 
 @Service
 public class FaqService {
     final private FaqAnswers faqAnswers = new FaqAnswers();
+    final private Pattern nonAsciiPattern = Pattern.compile("[^\\p{ASCII}]");
+    final private Pattern specialCharsPattern = Pattern.compile("[/:;?!]");
+    final private Pattern cedillaPattern = Pattern.compile("ç");
 
     public String getAnswer(String question) {
-        List<String> wordsList = Arrays.asList(question.toLowerCase().split("\\s+"));
+        String cleanedQuestion = cleanQuestion(question);
+        Set<String> wordsSet = new HashSet<>(Arrays.asList(cleanedQuestion.toLowerCase().split("\\s+")));
+
         return faqAnswers.getAnswers().stream()
-                .filter(entry -> entry.getKeywords().stream().anyMatch(wordsList::contains))
+                .filter(entry -> entry.getKeywords().stream().anyMatch(keyword -> wordsSet.contains(keyword.toLowerCase())))
                 .findFirst()
                 .map(FaqAnswer::getAnswer)
                 .orElse(faqAnswers.getDefaultAnswer());
+    }
+
+    private String cleanQuestion(String question) {
+        String normalized = Normalizer.normalize(question, Normalizer.Form.NFD);
+        String accentRemoved = nonAsciiPattern.matcher(normalized).replaceAll("");
+        String cedillaReplaced = cedillaPattern.matcher(accentRemoved).replaceAll("c");
+        return specialCharsPattern.matcher(cedillaReplaced).replaceAll("");
     }
 }
